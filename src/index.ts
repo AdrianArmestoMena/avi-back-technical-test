b8b9adc42b55f55a33164c6dfb8c81f73f55a04e
Adrian Armesto <armestomena@gmail.com>
2023-02-21 18:11:10 +0100

    Create index folder and call start server and db connection

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-const hola = "hi;";
+import startServer from "./server/startServer";
+import dbConnection from "./database/dbConnection";
+import connection from "./database/config.db";
+import environment from "./loadEnvironment";
+import chalk from "chalk";
 
-export default hola;
+(async () => {
+  try {
+    await startServer(environment.port);
+    dbConnection(connection);
+    console.log(
+      chalk.bgGreen("Starting server and connecting to the database")
+    );
+    // eslint-disable-next-line no-unused-vars
+  } catch (error) {
+    console.log(chalk.red("Error while starting the server and the database"));
+    process.exit(5);
+  }
+})();
